72c15c6ebf9852c37cfce4a5d4b6509ee963382a
João Vitor Rezende Moura <72020085+VitorMours@users.noreply.github.com>
2025-10-14 10:36:14 +0000

    modifying the form method to post

--- project/settings.py
+++ project/settings.py
@@ -28,6 +28,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://localhost:8000',
+    'http://127.0.0.1:8000',
+    'https://animated-pancake-wxp9q67gwjjhv5pp-8000.app.github.dev',
+]
 
 # Application definition
 
